39b21570a2f161f547b077f047641c5ccd0c07df
John <john@office4.in>
2023-10-06 11:28:41 +0530

    Syncfusion license read from a file

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,5 +9,26 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+string LicenseKey = string.Empty;
+try
+{
+    /// Read SyncFusion Licensekey from "syncfusionlicensekey.json"
+    HttpClient? http = new HttpClient()
+    {
+        BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+    };
+    builder.Services.AddScoped(sp => http);
+    using var response = await http.GetAsync("syncfusionlicensekey.json");
+    using var stream = await response.Content.ReadAsStreamAsync();
+    builder.Configuration.AddJsonStream(stream);
+    LicenseKey = builder.Configuration.GetValue<string>("SyncFusionLicenseKey");
+}
+finally
+{
+    Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense(LicenseKey);
+}
+
+
+
 builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
